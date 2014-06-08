860970715eb1c9bfeb5027deec35376e24de410f
hamwinkel <hamwinkel@gmail.com>
2014-06-08 19:04:41 -0400

    fopen with checks

---
+++ active/Carhealth.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (void)
+{
+
+    char screen[10,10];
+
+    screen [0,0] = 1;
+    screen [0,9] = 2;
+    screen [9,0] = 3;
+    screen [9,9] = 4;
+
+    int x = 0,
+        y = 0;
+
+    while (y < 10){
+        if (y = 9) printf("\n")
+        else putchar = screen [x, y];
+        while (x < 10){
+            if (x != '\0'){
+                putchar(screen[x, y]);
+            }
+            else if (x = 9) printf("\n");
+            x++;
+        }
+    y++;
+    }
+return 0;
+}
---
+++ active/Qengine.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (void)
+{
+    int len;
+    char ch, file_name[25];
+    FILE *fp;
+    system("ls");
+    printf("Enter the name of file you wish to open...\n");
+    fgets(file_name, 255, stdin);
+
+    /* Check for '\n' fopen returns NULL unless replaced with 0 */
+    len = strlen(file_name);
+    if (file_name[len-1] == '\n')
+    file_name[len-1] = 0;
+
+    fp = fopen(file_name,"r"); // read mode
+
+    if( fp == NULL )
+    {
+      perror("Error while opening the file.\n");
+      printf("%s", file_name);
+      exit(EXIT_FAILURE);
+    }
+
+    printf("The contents of %s file are :\n", file_name);
+
+    while( ( ch = fgetc(fp) ) != EOF )
+      printf("%c",ch);
+
+    fclose(fp);
+
+return 0;
+}
